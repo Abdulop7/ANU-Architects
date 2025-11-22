4efeeb1ed8156efd6ac566d2fb7bb8666572cf8c
Abdul Saboor <abdulsaboora691@gmail.com>
2025-11-22 18:40:14 +0500

    made the projects page dynamic

--- src/app/(public)/blogs/page.js
+++ src/app/(public)/blogs/page.js
@@ -3,6 +3,13 @@ import BlogsHero from "../../../../components/blogs/hero";
 import articlesData from "../../articles.json";
 import BlogGrid from "../../../../components/blogs/blog-grid";
 
+
+export const metadata = {
+  title: "Blogs | Architecture, Construction & Interior Design Insights in Multan, Pakistan | ANU Architects",
+  description: "Explore the latest blogs from ANU Architects, a leading architectural design and construction firm based in Multan, Pakistan. Discover expert insights on modern homes, commercial architecture, luxury interiors, and real estate trends—crafted to inspire, educate, and guide your next design journey with clarity and excellence.",
+};
+
+
 export default function BlogsPage() {
       const blogs = articlesData;
 
