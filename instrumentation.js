4ecac7408fa3164b7df63f8d79376aff4edcfda6
Abdul Saboor <abdulsaboora691@gmail.com>
2026-07-06 20:05:25 +0500

    fix

--- instrumentation.js
+++ instrumentation.js
@@ -1,25 +1,25 @@
-import { OTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-http'
-import { resourceFromAttributes } from '@opentelemetry/resources'
-import { LoggerProvider, SimpleLogRecordProcessor } from '@opentelemetry/sdk-logs'
+// import { OTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-http'
+// import { resourceFromAttributes } from '@opentelemetry/resources'
+// import { LoggerProvider, SimpleLogRecordProcessor } from '@opentelemetry/sdk-logs'
 
-export function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
-    const exporter = new OTLPLogExporter({
-      url: 'https://us.i.posthog.com/otlp/v1/logs',
-      headers: {
-        Authorization: 'Bearer phc_wYZB27SDUFgWMUnWmFbVjBF3CEDyNGDgbpkeFHGvPoRn',
-      },
-    })
+// export function register() {
+//   if (process.env.NEXT_RUNTIME === 'nodejs') {
+//     const exporter = new OTLPLogExporter({
+//       url: 'https://us.i.posthog.com/otlp/v1/logs',
+//       headers: {
+//         Authorization: 'Bearer phc_wYZB27SDUFgWMUnWmFbVjBF3CEDyNGDgbpkeFHGvPoRn',
+//       },
+//     })
 
-    const loggerProvider = new LoggerProvider({
-      resource: resourceFromAttributes({
-        'service.name': 'my-nextjs-app',
-      }),
-    })
+//     const loggerProvider = new LoggerProvider({
+//       resource: resourceFromAttributes({
+//         'service.name': 'my-nextjs-app',
+//       }),
+//     })
 
-    loggerProvider.addLogRecordProcessor(new SimpleLogRecordProcessor(exporter))
+//     loggerProvider.addLogRecordProcessor(new SimpleLogRecordProcessor(exporter))
 
-    // make the logger available globally
-    ;(globalThis).__posthogLogger = loggerProvider.getLogger('my-nextjs-app')
-  }
-}
+//     // make the logger available globally
+//     ;(globalThis).__posthogLogger = loggerProvider.getLogger('my-nextjs-app')
+//   }
+// }
